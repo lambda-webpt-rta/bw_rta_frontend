c86a58980c837cdb061f33617ab98a1e207d1ea6
jtwray <tuckerwray84@gmail.com>
2019-09-20 20:19:41 -0400

    Updates Signup Form To withFormik HOC. Axios post to reqres.in to simulate register-successful,Yup validation,Formik error boundary, Labels for A11y

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,70 +1,80 @@
-import React, {useState} from "react";
-import { withFormik, Form, Field } from 'Formik';
+import React, {useState,useEffect} from "react";
+import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-
-export const SignUpForm=( props ) => {
-    
-    const initialUser={email: '', password: '', username: '', }
+  
+export const SignUpForm=( { values, errors, touched, isSubmitting,users,setUsers,status,submitForm,handleChange },props) => {
+  const initialUser={email: '', password: '', username: '', }
     const [newUser, setNewUser]=useState( initialUser );
     
-    const handleChange=( event ) => {
-        setNewUser( {...newUser, [event.target.name]: event.target.value} );
-    }
-    const registerUser=( event ) => {
-        event.preventDefault();
-        console.log( newUser, props );
+  useEffect(()=>{
+      if(status){
         
-    }
-
+      }
+    },[status])
 
 
     return (
-        <Form onSubmit={registerUser}>
-            <label>Email
-            <Field type="text" name="email" placeholder="email"  value={newUser.email} />
-            </label>
-            <label>Password
-            <Field type="text" name="password" placeholder="password"  value={newUser.password} />
-            </label>
-            <label>UserName
-            <Field type="text" name="username" placeholder="username" value={newUser.username} />
-            </label>
-            <button type="submit">Submit</button>
+        <Form >
+            
+            
+            <label>Username</label>
+            <Field type="username" name="username" placeholder="username"  />
+            <label>Email</label>
+        {initialUser.email && initialUser.email && <p>{initialUser.email}</p>}
+            <Field type="email" name="email" placeholder="email"    />
+            <label>Password</label>
+            {initialUser.password && initialUser.password && <p>{initialUser.password}</p>}
+            <Field type="password" name="password" placeholder="password"   />
+           
+            <button type="submit" disabled={isSubmitting} >Register</button>
 
         </Form>
-    )
-};
+    );
+}
 
-export default withFormik({
+
+    const FormikSignupForm = withFormik({
     mapPropsToValues: (props) => {
-      return {
-        email: props.email || '',
-        password: props.password || '',
-        username: props.username || '',
-      }
-    },
+        return {
+            email: props.email || '',
+            password: props.password || '',
+            username: props.username || '',
+        }
+    }, 
 
     validationSchema: Yup.object().shape({
         username: Yup.string()
-          .required("NAME is required"),
-          email: Yup.string()
-          .required("EMAIL is required"),
+            .required("NAME is required"),
+        email: Yup.string()
+            .required("EMAIL is required"),
         password: Yup.string()
-          .min(6, "Password must be 6 characters or longer")
-          .required("Password is required")
-      }),
-    
-      handleSubmit: (values, { setStatus }) => {
-        axios.post('https://lambda-webpt-rta-api.herokuapp.com/', values)
-          .then((res) => {
+            .min(6, "Password must be 6 characters or longer")
+            .required("Password is required")
+    }),
+
+    handleSubmit(values,{ resetForm, setErrors, setSubmitting,setStatus,submitForm },emails) {
+      if (emails&&emails.includes(`${values.email}`)) {
+        setErrors({ email: "That email is already taken" });
+      } else {
+        axios.post("https://reqres.in/api/register", values)
+          .then(res => {
+            console.log(res,",`${res.data}`",`${res.data}`); // Data was created successfully and logs to console
+            resetForm();
+            setSubmitting(false);
+            submitForm(false)
             setStatus(res.data)
+            
           })
-          .catch((err) => {
-            console.log('Error:', err)
-          })
-      }
-    })(SignUpForm)
+          .catch(err => {
+            console.log(err); // There was an error creating the data and logs to console
+            setSubmitting(false);
+          });
+        }
+    }
+})(SignUpForm);
+
+export default FormikSignupForm; 
     
     
