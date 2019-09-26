d0dca78a586aec528099b252a05721516d593b3e
jtwray <tuckerwray84@gmail.com>
2019-09-25 23:31:11 -0400

    Upload/DropZone.js

---
+++ src/Components/Upload/Upload.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './Upload.css'
+
+class Upload extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="Upload">
+        <span className="Title">Upload Files</span>
+        <div className="Content">
+          <div>
+            <Dropzone
+              onFilesAdded={this.onFilesAdded}
+              disabled={this.state.uploading || this.state.successfullUploaded}
+            />
+          </div>
+          <div className="Files">
+            {this.state.files.map(file => {
+              return (
+                <div key={file.name} className="Row">
+                  <span className="Filename">{file.name}</span>
+                  {this.renderProgress(file)}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="Actions">{this.renderActions()}</div>
+      </div>
+    );
+  }
